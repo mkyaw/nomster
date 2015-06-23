f5ee9badb54a2f109d2c724e60a5acb1efae1d78
Myo Kyaw <myotkyaw@gmail.com>
2015-06-23 21:47:50 +0000

    Validate place name

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -11,8 +11,12 @@ class PlacesController < ApplicationController
 	end
 
 	def create
-		current_user.places.create(p_params)
-		redirect_to root_path
+		@place = current_user.places.create(p_params)
+		if @place.valid?
+			redirect_to root_path
+		else
+			render :new, :status => :unprocessable_entity
+		end
 	end
 
 	def show
@@ -35,7 +39,11 @@ class PlacesController < ApplicationController
 
 
 		@place.update_attributes(p_params)
-		redirect_to root_path
+		if @place.valid?
+			redirect_to root_path
+		else
+			render :edit, :status => :unprocessable_entity
+		end
 	end
 
 	def destroy
