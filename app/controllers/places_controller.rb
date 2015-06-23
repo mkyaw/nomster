dc93fcb109081543ce805d1a34b750180ecb33e8
Myo Kyaw <vagrant@precise32.(none)>
2015-06-23 00:27:37 +0000

    Build a form to save new places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,7 +1,13 @@
 class PlacesController < ApplicationController
 
 	def index
-		@places = Place.all
+		# @posts = Place.all
+		@places = Place.paginate(:page => params[:page])
+
+	end
+
+	def new
+		@place = Place.new
 	end
 
 end
