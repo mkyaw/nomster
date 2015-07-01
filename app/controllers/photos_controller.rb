d4e4d043f1f71ea35dbffb2ff04ece5a7f309519
Myo Kyaw <myotkyaw@gmail.com>
2015-07-01 08:40:47 +0000

    Add photo form on place show page and upload photo button

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,17 +1,27 @@
 class PhotosController < ApplicationController
+	# before filter
 	before_action :authenticate_user!
-
+	
+	# create action for Photos controller
 	def create
+		# @place is assigned to Place by place_id
 		@place = Place.find(params[:place_id])
 		
-		@place.photos.create(pht_params.merge(:user => current_user))
-
+		# photo is created according to the parameters 
+		# set by pht_params below
+		# NO MORE => photo is also associated with current user
+		@place.photos.create(pht_params)
+		
+		# redirect to place page
 		redirect_to place_path(@place)
-
+		
 	end
 
 	private
+	# parameters set for photos that permit 
+	# only message and rating
 	def pht_params
 		params.require(:photo).permit(:caption)
 	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
 
   resources :places do
     resources :comments, :only => :create
+    resources :photos, :only => :create
   end
 
   # Example of regular route:
